c275f5b2fd06e74499ac9a9a73537aa5299d7b5c
CengizKocer <c-ngiz@live.nl>
2023-04-01 12:54:44 +0200

    Add files via upload

---
+++ fxmanifest.lua
@@ -0,0 +1,19 @@
+fx_version 'cerulean'
+game 'gta5'
+
+name "qb-ammu"
+description "aaa"
+author "aa"
+version "a"
+
+shared_scripts {
+	'shared/*.lua'
+}
+
+client_scripts {
+	'client/*.lua'
+}
+
+server_scripts {
+	'server/*.lua'
+}
